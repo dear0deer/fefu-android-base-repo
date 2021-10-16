72760749532e53d9c6f39f99ed706532dd524142
Olga <lyalya_lel@mail.ru>
2021-10-16 23:02:59 +1000

    16.10.2021

---
+++ app/src/main/java/ru/fefu/activitytracker/MainActivity.kt
@@ -0,0 +1,21 @@
+package ru.fefu.activitytracker
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+    }
+
+
+}
