97242bb7d59f042be1d9181e358038928c5ca332
Olga <lyalya_lel@mail.ru>
2021-10-30 19:12:44 +1000

    Homework 1-2

--- app/src/main/java/ru/fefu/activitytracker/homework1/SecondActivity.kt
+++ app/src/main/java/ru/fefu/activitytracker/homework1/SecondActivity.kt
@@ -1,21 +1,13 @@
-package ru.fefu.activitytracker
+package ru.fefu.activitytracker.homework1
 
-import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import ru.fefu.activitytracker.R
 
-class MainActivity : AppCompatActivity() {
-
-
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-
     }
-
-
 }
